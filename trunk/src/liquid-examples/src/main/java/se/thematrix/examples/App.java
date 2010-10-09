958719f0a22341985d16a0c9c32bb62768bffa00
mikael@thematrix.se <mikael@thematrix.se@8ddf09ff-f88d-5742-63e9-b74fe226304b>
2010-10-09 10:24:48 +0000

    Added folder structure and maven liquid-examples project

---
+++ trunk/src/liquid-examples/src/main/java/se/thematrix/examples/App.java
@@ -0,0 +1,13 @@
+package se.thematrix.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
