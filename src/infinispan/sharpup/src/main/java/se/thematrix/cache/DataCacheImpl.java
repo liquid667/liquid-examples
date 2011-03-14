4a0c989a6780f7999b3a577aba1babf02d5f4bf2
computersaysno2@gmail.com <computersaysno2@gmail.com@8ddf09ff-f88d-5742-63e9-b74fe226304b>
2011-03-14 21:30:03 +0000

    Added infinispan-query to the mix.

--- src/infinispan/sharpup/src/main/java/se/thematrix/cache/DataCacheImpl.java
+++ src/infinispan/sharpup/src/main/java/se/thematrix/cache/DataCacheImpl.java
@@ -1,14 +1,21 @@
 package se.thematrix.cache;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
 
+import org.apache.lucene.queryParser.ParseException;
 import org.infinispan.Cache;
+import org.infinispan.query.CacheQuery;
+import org.infinispan.query.QueryFactory;
+import org.infinispan.query.backend.QueryHelper;
 
 import se.thematrix.model.User;
 
 public class DataCacheImpl implements DataCache {
 	
 	private final static Cache<Integer,User> cache = MyCacheContainer.getCache("Users");
+	private final static QueryHelper qh = new QueryHelper(cache, new Properties(), User.class);
 	
 	@Override
 	public int size() {
@@ -33,6 +40,18 @@ public class DataCacheImpl implements DataCache {
 	@Override
 	public User get(Integer key) {
 		return cache.get(key);
+	}
+	
+	public boolean checkLogin(String userName, String password){
+		QueryFactory qf = new QueryFactory(cache, qh);
+		try {
+			CacheQuery cq = qf.getBasicQuery("userName", userName);
+			int hits = cq.getResultSize();
+			if(hits > 0) return true;
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 	
 }
